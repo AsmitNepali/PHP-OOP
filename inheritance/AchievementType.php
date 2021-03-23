2010cb051171a1b7b2d5eb24415b77864f760bba
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-23 18:37:20 +0545

    [IMP] PHP-OOP: Inheritance Override.

---
+++ inheritance/AchievementType.php
@@ -0,0 +1,32 @@
+<?php
+
+    class AchievementType
+    {
+        public function name()
+        {
+            // Achievement Type 
+        }
+
+        public function difficulty()
+        {
+            return "Intermediate";
+        }
+
+        public function icon()
+        {
+            
+        }
+    }
+
+    class FirstThousandPoints extends AchievementType
+    {
+        public function qualifier($user)
+        {
+
+        }
+
+        public function name()
+        {
+            
+        }
+    }
